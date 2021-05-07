3e3d18c1832fa4a566a37f89eadcd8712212921c
goh-chunlin <chunlin@live.com.sg>
2021-05-07 20:05:56 +0800

    Added permission_test.go in the integration test.

---
+++ test/integration/permission_test.go
@@ -0,0 +1,33 @@
+// Copyright 2020 The go-onedrive AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a license that can be found in the LICENSE file.
+
+package integration
+
+import (
+	"testing"
+)
+
+func TestPermission_CreateAnynomousViewLink(t *testing.T) {
+	// ctx, client := setup()
+
+	// permission, err := client.DrivePermissions.CreateShareLink(ctx, "<<input>>", "view", "anonymous")
+	// if err != nil {
+	// 	fmt.Printf("Error: %v\n", err)
+	// 	return
+	// }
+	// fmt.Printf("Result: %v\n", permission.Link.Type+"; "+permission.Link.Scope+"; "+permission.Link.URL)
+}
+
+func TestPermission_List(t *testing.T) {
+	// ctx, client := setup()
+
+	// permissions, err := client.DrivePermissions.List(ctx, "<<input>>")
+	// if err != nil {
+	// 	fmt.Printf("Error: %v\n", err)
+	// 	return
+	// }
+	// for _, permission := range permissions {
+	// 	fmt.Printf("Results: %v\n", permission.Link.Type+"; "+permission.Link.Scope+"; "+permission.Link.URL)
+	// }
+}
